aa5eda410906023eed225aaa389ec406142e61bd
Miracle Friday <miraclef60@gmail.com>
2024-10-26 13:37:59 +0100

    feat: Add not-found component

---
+++ src/components/not-found.tsx
@@ -0,0 +1,16 @@
+import { Main, Section, Container } from "@/components/craft";
+
+
+export function NotFound() {
+
+
+  return (
+    <Main>
+      <Section>
+        <Container>
+          <h1>New not found page under construction</h1>  
+        </Container>  
+      </Section>
+    </Main>
+  )
+}
