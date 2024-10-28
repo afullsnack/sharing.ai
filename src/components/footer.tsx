95d2f6d9caf4c6e728b90f667bea056a14de7c05
Miracle Friday <miraclef60@gmail.com>
2024-10-28 13:21:39 +0100

    refactor: Refactor components and add footer and DefaultCatchBoundary

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+import { Container, Main, Section } from "./craft";
+
+function Footer() {
+  return (
+    <Main>
+      <Section className="w-full !p-0 border">
+        <Container className="!p-0 py-3 w-full">
+          <h1>Footer for mankind</h1>
+        </Container>
+      </Section>
+    </Main>
+  )
+}
+
+export default Footer;
