0a190508d865b17dc5f946c0aa8a9c42cfc09426
Miracle Friday <miraclef60@gmail.com>
2024-10-28 13:22:41 +0100

    refactor: Update `createTanStackRouter` function config

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 // app/router.tsx
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
+import { NotFound } from './components/not-found'
 
 export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
+    defaultPreload: 'intent',
+    defaultErrorComponent: DefaultCatchBoundary,
+    defaultNotFoundComponent: () => <NotFound />
   })
 
   return router
