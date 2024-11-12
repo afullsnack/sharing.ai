3bbd985a237832cb766a12709007ffa5a054a815
Miracle Friday <miraclef60@gmail.com>
2024-11-12 01:52:54 +0100

    feat: Make prompt suggestions carousel and screen adaptable

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import { Container, Section } from "./craft";
 function Footer() {
   return (
     <Section className="w-full !p-0 border">
-      <Container className="!p-0 py-5 w-full lg:max-w-3xl">
+      <Container className="!p-0 py-5 px-3 w-full lg:max-w-3xl">
         <p className="text-muted-foreground">@<a href="https://afullsnack.dev" target="_blank">afullsnack</a>{" "}All rights reserved. 2024-present</p>
       </Container>
     </Section>
