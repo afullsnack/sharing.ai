aa9b910e9b04b6a58218d8ea1c5e5f223a14c4b1
Miracle Friday <miraclef60@gmail.com>
2025-02-16 14:37:48 +0100

    refactor: handle authorization in `_authed` route

--- app/routes/_authed.tsx
+++ app/routes/_authed.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/start';
 import { Schema } from '@effect/schema';
+import { getRequestHeaders, setHeaders } from '@tanstack/start/server';
+import { loggerMiddleware } from '@/middleware/logger';
 
 
 const FunctionInput = Schema.Struct({
@@ -11,23 +13,26 @@ type IFunctionInput = typeof FunctionInput.Type;
 
 const login = createServerFn({ method: "POST" })
   .validator(Schema.decodeSync(FunctionInput))
-  .handler(async ({data, method}) => {
+  .handler(async ({ data, method }) => {
     return data;
   });
 
+
+const validate = createServerFn({ method: 'POST' })
+  .validator((data: {name:string}) => data)
+  .middleware([loggerMiddleware])
+  .handler(async () => {
+    const headers = getRequestHeaders();
+    throw redirect({ to: '/' })
+  })
+
 // TODO: implement route guards
 export const Route = createFileRoute('/_authed')({
-  beforeLoad: () => {
-    const isFalse = Math.random() * 100
-    if (isFalse) {
-      console.log('You are unauthorized to access this page');
-      throw redirect({ to: '/' })
-    }
-  },
-  loader: async () => await login({
-    data: {
-      name: 'afullsnack',
-      age: 10,
-    }
-  })
+  beforeLoad: async () => validate({data: {name: 'Jef'}}),
+  // loader: async () => await login({
+  //   data: {
+  //     name: 'afullsnack',
+  //     age: 10,
+  //   }
+  // })
 })
