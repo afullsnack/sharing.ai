3b7f849231e532113879041e514e2bb8f9de882e
Miracle Friday <miraclef60@gmail.com>
2025-05-14 17:39:35 +0100

    refactor: Add parent chat component inside

--- app/routes/_authed/~/$id.$tab.tsx
+++ app/routes/_authed/~/$id.$tab.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { observer } from '@legendapp/state/react'
 import { Section } from '@/components/craft'
+import Chat from './-components/Chat'
 
 export const Route = createFileRoute('/_authed/~/$id/$tab')({
   component: observer(Spndl),
@@ -9,6 +10,15 @@ export const Route = createFileRoute('/_authed/~/$id/$tab')({
 function Spndl() {
   const params = Route.useParams()
   console.log(params, ':::params')
+
+  if(params.tab === "chat") {
+    return (
+      <Section className='!p-0'>
+        <Chat />
+      </Section>
+    )
+  }
+  
   return (
     <Section className='!p-0'>
       <pre>{JSON.stringify({ params }, null, 4)}</pre>
---
+++ app/routes/_authed/~/-components/Chat.tsx
@@ -0,0 +1,112 @@
+import { ChatBubble, ChatBubbleAvatar, ChatBubbleMessage } from "@/components/ai-chat/chat-bubble"
+import { ChatMessageList } from "@/components/ai-chat/chat-message-list"
+import { Container } from "@/components/craft"
+import { Card, CardContent } from "@/components/ui/card"
+
+interface IChatProps { }
+
+
+const messages = [
+  {
+    id: 1,
+    message: 'Test message',
+    sender: 'ai'
+  },
+  {
+    id: 2,
+    message: 'Request message for ai to carry out tasks',
+    sender: 'user'
+  },
+  {
+    id: 3,
+    message: 'Follow up message',
+    sender: 'user'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+  {
+    id: 4,
+    message: 'Long Test message',
+    sender: 'ai'
+  },
+]
+export default function Chat(props: IChatProps) {
+  console.log('Chat props', props)
+
+  return (
+    <Container className="h-[800px] !px-0">
+      <Card>
+        <CardContent className="bg-muted-forground">
+          <ChatMessageList>
+            {
+              messages.map((message) => (
+                <ChatBubble
+                  key={message.id}
+                  variant={message.sender === "user" ? 'sent' : 'received'}
+                >
+                  <ChatBubbleAvatar
+                    className="h-8 w-8 shrink-0"
+                    // src={
+                    //   message.sender === "user"
+                    //     ? "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=64&h=64&q=80&crop=faces&fit=crop"
+                    //     : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=64&h=64&q=80&crop=faces&fit=crop"
+                    // }
+                    fallback={message.sender === "user" ? "US" : "AI"}
+                  />
+                  <ChatBubbleMessage
+                    variant={message.sender === "user" ? "sent" : "received"}
+                  >
+                    {message.message}
+                  </ChatBubbleMessage>
+                </ChatBubble>
+              ))
+            }
+          </ChatMessageList>
+        </CardContent>
+      </Card>
+    </Container>
+  )
+} 
